88eaff861af1fde9bce6b1f7de693fc139f4f7ff
Keaton Freude <freudek90@gmail.com>
2018-08-24 15:56:34 -0700

    Fix up formatting, add 'add-score' component. add angular material and material typography

---
+++ pool-frontend/src/app/add-score/add-score.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pa-add-score',
+  templateUrl: './add-score.component.html',
+  styleUrls: ['./add-score.component.css']
+})
+export class AddScoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- pool-frontend/src/app/app.module.ts
+++ pool-frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppComponent } from "./app.component";
 import { UserScoreComponent } from "./user-score/user-score.component";
 import { GameScoreComponent } from './game-score/game-score.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddScoreComponent } from './add-score/add-score.component';
 
 @NgModule({
-    declarations: [AppComponent, UserScoreComponent, GameScoreComponent],
-    imports: [BrowserModule, FlexLayoutModule],
+    declarations: [AppComponent, UserScoreComponent, GameScoreComponent, AddScoreComponent],
+    imports: [BrowserModule, FlexLayoutModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
