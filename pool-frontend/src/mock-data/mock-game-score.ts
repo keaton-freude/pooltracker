0d85a17145485bf48e02010ff2eaec1bded608b1
Keaton Freude <freudek90@gmail.com>
2018-08-24 13:36:14 -0700

    Add mocks for game score and user score

---
+++ pool-frontend/src/mock-data/mock-game-score.ts
@@ -0,0 +1,6 @@
+import { GameScore } from "../app/models/game-score";
+
+export const mockGameScore: GameScore = {
+    chrisScore: 9,
+    keatonScore: 41
+};
---
+++ pool-frontend/src/mock-data/mock-user-score.ts
@@ -0,0 +1,13 @@
+import { UserScore } from "../app/models/user-score";
+
+export const mockUser1: UserScore = {
+    name: "Keaton",
+    wins: 30,
+    losses: 7
+};
+
+export const mockUser2: UserScore = {
+    name: "Chris",
+    wins: 7,
+    losses: 30
+};
