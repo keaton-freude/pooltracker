dfee37ed93d48fe5187ace62aae1fe7379ee20b8
Keaton Freude <freudek90@gmail.com>
2018-08-29 22:18:00 -0700

    Add current game score tracking

--- src/app/services/scores.service.ts
+++ src/app/services/scores.service.ts
@@ -3,22 +3,48 @@ import { HttpClient } from "selenium-webdriver/http";
 import { Score } from "../models/score";
 import { GetBackendHttpRoute } from "../utils/route-builder";
 import { BackendApi } from "./backend-api.service";
+import { GameScore } from "../models/game-score";
+import { Subject, Observable } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class ScoresService {
     constructor(private api: BackendApi) {}
-
-    // submitScore(score: AddScore) {
-    //     this.http.post()
-    // }
+    private currentScore: Subject<GameScore> = new Subject<GameScore>();
 
     public getAllScores(): Promise<Score[]> {
         return this.api.get<Score[]>("/scores").toPromise();
     }
 
-    public submitScore(score: Score): Promise<Object> {
-        return this.api.post<Score>("/scores", score).toPromise();
+    public async submitScore(score: Score): Promise<Object> {
+        return new Promise<Object>((resolve, reject) => {
+            this.api
+                .post<Score>("/scores", score)
+                .toPromise()
+                .then(() => {
+                    this.updateCurrentScore();
+                    resolve();
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
+    }
+
+    public getCurrentScore(): Observable<GameScore> {
+        return this.currentScore.asObservable();
+    }
+
+    public updateCurrentScore() {
+        this.api
+            .get<GameScore>("/wins")
+            .toPromise()
+            .then(score => {
+                this.currentScore.next(score);
+            })
+            .catch(err => {
+                console.log("Error getting game score...");
+            });
     }
 }
