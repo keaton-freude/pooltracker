bd4f657d9f3bad05582874feeb66d7a13d6f2cfb
Keaton Freude <freudek90@gmail.com>
2018-09-03 14:48:14 -0700

    Upgrade npm packages. Ugrade TS to 2.9.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  production: false,
-  BACKEND_API_HOST: 'http://localhost',
-  BACKEND_API_PORT: '3000'
+  production: true,
+  BACKEND_API_HOST: 'undefined',
+  BACKEND_API_PORT: 'undefined'
 };
