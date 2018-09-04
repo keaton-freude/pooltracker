ec2e331232084a19917e4e871c9a3caab01b8ac8
Keaton Freude <freudek90@gmail.com>
2018-09-04 10:16:17 -0700

    Update the set-env script to not require dotenv

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -1,10 +1,6 @@
 import { writeFile } from "fs";
 import { argv } from "yargs";
 
-// This is good for local dev environments, when it's better to
-// store a projects environment variables in a .gitignore'd file
-require("dotenv").config();
-
 // Would be passed to script like this:
 // `ts-node set-env.ts --environment=dev`
 // we get it from yargs's argv object
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  BACKEND_API_HOST: 'http://192.168.1.115',
+  BACKEND_API_HOST: 'http://127.0.0.1',
   BACKEND_API_PORT: '3000'
 };
